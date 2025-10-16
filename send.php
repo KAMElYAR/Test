a8c5ea4c5e51d6e33ecd31280971a5358c03658a
KAMElYAR <amirking138558138558@gmail.com>
2025-10-16 16:36:21 +0330

    Update send.php

--- send.php
+++ send.php
@@ -1,5 +1,23 @@
 <?php
-    sleep(2);
-    $msg = urlencode(file_get_contents("out.txt"));
-    file_get_contents("https://api.telegram.org/bot8223992925:AAE8R0yTj540UaQ1duMOPpfC7PsC3Z-j5os/sendMessage?chat_id=1196450049&text=$msg");
+$bot_token = "<8223992925:AAE8R0yTj540UaQ1duMOPpfC7PsC3Z-j5os>";      // مثل 123456:ABC-...
+$chat_id   = "<1196450049>";        // عدد چت یا یوزر
+$text      = "سلام! تست ربات از طریق PHP.";
+
+$api_url = "https://api.telegram.org/bot{$bot_token}/sendMessage";
+$post_fields = [
+    'chat_id' => $chat_id,
+    'text'    => $text,
+];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $api_url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$res = curl_exec($ch);
+$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+echo "HTTP code: $httpcode\n";
+echo "Response: $res\n";
 ?>
